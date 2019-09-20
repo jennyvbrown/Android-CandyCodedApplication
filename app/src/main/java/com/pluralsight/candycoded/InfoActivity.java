f63d7a92126757e38cca61c3e927ade7eb137c86
Jenny Brown <jvbrown@control4.com>
2019-09-20 11:28:28 -0600

    onOptionsSelected Override complete.

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -1,11 +1,15 @@
 package com.pluralsight.candycoded;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
+
+import java.net.URI;
 
 public class InfoActivity extends AppCompatActivity {
 
@@ -21,6 +25,16 @@ public class InfoActivity extends AppCompatActivity {
                 into(candyStoreImageView);
 
 
+    }
+
+    public void createMapIntent(View view){
+        Uri uriAddress = Uri.parse("geo;0,0?q=618 E South St Orlando, FL 32801");
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, uriAddress);
+        mapIntent.setPackage("com.google.android.apps.maps");
+
+        if(mapIntent.resolveActivity(getPackageManager()) != null) {
+            startActivity(mapIntent);
+        }
     }
 
     // ***
